de1dee04c1e206a3d62854744bab769f2291bf61
Saideep Samineni <73835168+saideep2000@users.noreply.github.com>
2024-07-06 14:49:59 -0400

    Time: 97 ms (28.93%), Space: 20.7 MB (38.84%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.py
+++ 0049-group-anagrams/0049-group-anagrams.py
@@ -1,20 +1,9 @@
-import collections as cl
 class Solution:
-    def get_key_by_value(d, value):
-        return next((key for key, val in d.items() if val == value), None)
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
         rem = {}
-        temp = {}
         for i in strs:
-            comp = dict(cl.Counter(i))
-            print(comp)
-            if comp in rem.values():
-                key = Solution.get_key_by_value(rem, comp)
-                temp[key].append(i)
+            if tuple(sorted(list(i))) in rem.keys():
+                rem[tuple(sorted(list(i)))].append(i)
             else:
-                rem[i] = comp
-                temp[i] = [i]
-        print(rem)
-        print(temp)
-        return temp.values()
-    
+                rem[tuple(sorted(list(i)))] = [i]
+        return rem.values()
