6bd2ab967b0dd0b0f8d2e2cb107cf26259023b31
Saideep Samineni <73835168+saideep2000@users.noreply.github.com>
2024-07-08 09:56:41 -0400

    Time: 319 ms (98.9%), Space: 31.8 MB (31.24%) - LeetHub

--- 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.py
+++ 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.py
@@ -1,23 +1,15 @@
 class Solution:
-    def rec(rem, i):
-        curr = rem[i]
-        if i-1 in rem:
-            if rem[i-1] == 0:
-                rem,curr = Solution.rec(rem,i-1)
-            else:
-                curr = rem[i-1]
-        rem[i] = curr + 1
-        return rem,rem[i]
     def longestConsecutive(self, nums: List[int]) -> int:
-        rem = {}
-        curr = 0
+        rem = set(nums)
         m = 0
-        for i in nums:
-            if i not in rem:
-                rem[i] = 0
         for i in rem:
-            if rem[i] == 0:
-                rem,curr = Solution.rec(rem, i)
-            if m < curr:
-                m = curr
+            if i-1 in rem:
+                continue
+            else:
+                seq = 0
+                while i+seq in rem:
+                    seq = seq + 1
+                if m < seq:
+                    m = seq
         return m
+                
