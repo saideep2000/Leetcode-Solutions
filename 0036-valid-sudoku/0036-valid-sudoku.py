725a64fe17a89d2d9640c68fd235ca754098e568
Saideep Samineni <73835168+saideep2000@users.noreply.github.com>
2024-07-07 21:58:52 -0400

    Time: 153 ms (5.05%), Space: 16.7 MB (13.03%) - LeetHub

---
+++ 0036-valid-sudoku/0036-valid-sudoku.py
@@ -0,0 +1,17 @@
+class Solution:
+    def isValidSudoku(self, board: List[List[str]]) -> bool:
+        row = collections.defaultdict(set)
+        col = collections.defaultdict(set)
+        s = collections.defaultdict(set)
+        for i in range(0,9):
+            for j in range(0,9):
+                print(s)
+                if board[i][j] == ".":
+                    continue
+                elif board[i][j] in row[i] or board[i][j] in col[j] or board[i][j] in s[str(i//3) + " "+str(j//3)]:
+                    return False
+                else:
+                    row[i].add(board[i][j])
+                    col[j].add(board[i][j])
+                    s[str(i//3) + " "+str(j//3)].add(board[i][j])
+        return True
