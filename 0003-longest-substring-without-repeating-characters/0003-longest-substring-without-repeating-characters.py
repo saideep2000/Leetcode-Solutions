e9a98acd6fb8ceaec7d572a6ff0e3a364e506b40
Saideep Samineni <73835168+saideep2000@users.noreply.github.com>
2024-07-14 19:26:49 -0400

    Time: 700 ms (5%), Space: 16.7 MB (32.39%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.py
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.py
@@ -3,6 +3,7 @@ class Solution:
         rem = collections.defaultdict()
         i, j = 0, 0
         substring = 0
+        substring_len = 0
         while j < len(s):
             if s[j] not in rem.keys():
                 rem[s[j]] = j
@@ -10,6 +11,7 @@ class Solution:
                 i = rem[s[j]] + 1
                 j = rem[s[j]]
                 rem = collections.defaultdict()
-            substring = max(j - i + 1, substring)
+            substring_len = j - i + 1
+            substring = max(substring_len, substring)
             j = j + 1
         return substring
