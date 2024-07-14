d12eabda74ab8f6d507563ae8221a021f9ddbc69
Saideep Samineni <73835168+saideep2000@users.noreply.github.com>
2024-07-14 17:15:02 -0400

    Time: 86 ms (97.62%), Space: 18.4 MB (81.51%) - LeetHub

--- 0042-trapping-rain-water/0042-trapping-rain-water.py
+++ 0042-trapping-rain-water/0042-trapping-rain-water.py
@@ -11,7 +11,6 @@ class Solution:
                 temp = maxRight - height[j]
             if temp > 0:
                 s = s + temp
-                print(s)
             if maxLeft < height[i]:
                 maxLeft = height[i]
             if maxRight < height[j]:
@@ -21,23 +20,3 @@ class Solution:
             else:
                 j = j - 1
         return s
-
-
-        # minleftright = [float('inf')] * len(height)
-        # n = len(height)
-        # maxLeft = 0
-        # maxRight = 0
-        # minleftright[0] = 0
-        # minleftright[len(height)-1] = 0
-        # s = 0
-        # for i in range(n):
-        #     minleftright[i] = maxLeft
-        #     maxLeft = max(minleftright[i], height[i])
-        # for i in range(n):
-        #     minleftright[n-1-i] = min(maxRight, minleftright[n-1-i])
-        #     maxRight = max(minleftright[n-1-i], height[n-1-i])
-        # for i in range(len(minleftright)):
-        #     v = minleftright[i] - height[i]
-        #     if v > 0:
-        #         s = s + v
-        # return s
