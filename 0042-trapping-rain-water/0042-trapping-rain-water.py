a40cbd7980c06c8f80861659c4c3bd3ff75a6800
Saideep Samineni <73835168+saideep2000@users.noreply.github.com>
2024-07-14 17:14:47 -0400

    Time: 98 ms (70.49%), Space: 18.3 MB (81.51%) - LeetHub

--- 0042-trapping-rain-water/0042-trapping-rain-water.py
+++ 0042-trapping-rain-water/0042-trapping-rain-water.py
@@ -1,20 +1,43 @@
 class Solution:
     def trap(self, height: List[int]) -> int:
-        minleftright = [float('inf')] * len(height)
-        n = len(height)
         maxLeft = 0
         maxRight = 0
-        minleftright[0] = 0
-        minleftright[len(height)-1] = 0
         s = 0
-        for i in range(n):
-            minleftright[i] = maxLeft
-            maxLeft = max(minleftright[i], height[i])
-        for i in range(n):
-            minleftright[n-1-i] = min(maxRight, minleftright[n-1-i])
-            maxRight = max(minleftright[n-1-i], height[n-1-i])
-        for i in range(len(minleftright)):
-            v = minleftright[i] - height[i]
-            if v > 0:
-                s = s + v
+        i,j = 0, len(height) -1
+        while i<=j:
+            if maxLeft <= maxRight:
+                temp = maxLeft - height[i]
+            else:
+                temp = maxRight - height[j]
+            if temp > 0:
+                s = s + temp
+                print(s)
+            if maxLeft < height[i]:
+                maxLeft = height[i]
+            if maxRight < height[j]:
+                maxRight = height[j]
+            if maxLeft <= maxRight:
+                i = i + 1
+            else:
+                j = j - 1
         return s
+
+
+        # minleftright = [float('inf')] * len(height)
+        # n = len(height)
+        # maxLeft = 0
+        # maxRight = 0
+        # minleftright[0] = 0
+        # minleftright[len(height)-1] = 0
+        # s = 0
+        # for i in range(n):
+        #     minleftright[i] = maxLeft
+        #     maxLeft = max(minleftright[i], height[i])
+        # for i in range(n):
+        #     minleftright[n-1-i] = min(maxRight, minleftright[n-1-i])
+        #     maxRight = max(minleftright[n-1-i], height[n-1-i])
+        # for i in range(len(minleftright)):
+        #     v = minleftright[i] - height[i]
+        #     if v > 0:
+        #         s = s + v
+        # return s
