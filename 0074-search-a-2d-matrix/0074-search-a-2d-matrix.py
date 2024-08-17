38313fc68716dc119f126a0514730f91aa69fbb0
Saideep Samineni <73835168+saideep2000@users.noreply.github.com>
2024-08-17 18:39:30 -0400

    Time: 52 ms (15.04%), Space: 17 MB (89.67%) - LeetHub

---
+++ 0074-search-a-2d-matrix/0074-search-a-2d-matrix.py
@@ -0,0 +1,18 @@
+class Solution:
+    def binarySearch(target, li):
+        l = 0
+        r = len(li)-1
+        while l <= r :
+            m = (l+r)//2
+            if li[m] == target:
+                return True
+            elif li[m] > target:
+                r = m - 1
+            else:
+                l = m + 1
+        return False
+    def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
+        for i in range(0,len(matrix)):
+            if target >= matrix[i][0] and target <= matrix[i][len(matrix[i])-1]:
+                return Solution.binarySearch(target, matrix[i])
+        return False
