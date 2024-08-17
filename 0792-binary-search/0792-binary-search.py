726d2b03600c9bce58023256ac4979cb725a6586
Saideep Samineni <73835168+saideep2000@users.noreply.github.com>
2024-08-17 16:54:28 -0400

    Time: 189 ms (66.19%), Space: 18 MB (67%) - LeetHub

--- 0792-binary-search/0792-binary-search.py
+++ 0792-binary-search/0792-binary-search.py
@@ -1,13 +1,8 @@
 class Solution:
     def search(self, nums: List[int], target: int) -> int:
-        # if len(nums) == 1:
-        #     if nums[0] == target:
-        #         return 0
-        #     else:
-        #         return -1
+
         l = 0
         h = len(nums)-1
-     
         while l <= h:
             m = (h+l)//2
             if nums[l] == target:
@@ -16,14 +11,11 @@ class Solution:
                 return h
             if nums[m] == target:
                 return m
-                
             if nums[m] > target:
                 h = m - 1
                 l = l + 1
             else:
                 l = m + 1
                 h = h - 1
-
-
 
         return -1
