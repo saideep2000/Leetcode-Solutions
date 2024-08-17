b7a42ca4fd9e4c7b6badc23be491ccce3ca47f7d
Saideep Samineni <73835168+saideep2000@users.noreply.github.com>
2024-08-17 16:57:01 -0400

    Time: 184 ms (87.83%), Space: 18.1 MB (28.23%) - LeetHub

--- 0792-binary-search/0792-binary-search.py
+++ 0792-binary-search/0792-binary-search.py
@@ -5,17 +5,17 @@ class Solution:
         h = len(nums)-1
         while l <= h:
             m = (h+l)//2
-            if nums[l] == target:
-                return l
-            if nums[h] == target:
-                return h
+            # if nums[l] == target:
+            #     return l
+            # if nums[h] == target:
+            #     return h
             if nums[m] == target:
                 return m
             if nums[m] > target:
                 h = m - 1
-                l = l + 1
+                # l = l + 1
             else:
                 l = m + 1
-                h = h - 1
+                # h = h - 1
 
         return -1
