f22d740e0fcb26a46464601da872e7632836c759
Saideep Samineni <73835168+saideep2000@users.noreply.github.com>
2024-11-14 18:32:15 -0500

    Time: 3 ms (25.57%), Space: 17.4 MB (33.67%) - LeetHub

--- 0102-binary-tree-level-order-traversal/0102-binary-tree-level-order-traversal.py
+++ 0102-binary-tree-level-order-traversal/0102-binary-tree-level-order-traversal.py
@@ -6,38 +6,18 @@
 #         self.right = right
 class Solution:
     def levelOrder(self, root: Optional[TreeNode]) -> List[List[int]]:
-        return Solution.rec(self, root)
-    def rec(self, root):
-        if root == None:
-            return []
-        temp = [root.val]
-        merge = []
-        merge.append(temp)
-        temp1 = Solution.rec(self,root.left)
-        temp2 = Solution.rec(self,root.right)
-        if temp1 == []:
-            if temp2 == []:
-                return merge
-            else:
-                return merge + temp2
-        else:
-            if temp2 == []:
-                return merge + temp1
-            else:
-                temp1 = Solution.combine_lists(self, temp1, temp2)
-                return merge + temp1
-    def combine_lists(self, list1, list2):
-        combined = []
-        max_len = max(len(list1), len(list2))
-        
-        for i in range(max_len):
-            if i < len(list1) and i < len(list2):
-                combined.append(list1[i] + list2[i])
-            elif i < len(list1):
-                combined.append(list1[i])
-            else:
-                combined.append(list2[i])
-                
-        return combined
-        
-        
+        res = []
+        q = collections.deque()
+        q.append(root)
+        while q:
+            qlen = len(q)
+            level = []
+            for i in range(qlen):
+                temp = q.popleft()
+                if temp:
+                    level.append(temp.val)
+                    q.append(temp.left)
+                    q.append(temp.right)
+            if level:
+                res.append(level)
+        return res
