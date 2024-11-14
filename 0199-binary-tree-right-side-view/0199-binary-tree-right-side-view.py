a6b03ed7888530e756c15b88abbfcd73caa3cab6
Saideep Samineni <73835168+saideep2000@users.noreply.github.com>
2024-11-14 18:46:26 -0500

    Time: 0 ms (100%), Space: 16.6 MB (32.26%) - LeetHub

--- 0199-binary-tree-right-side-view/0199-binary-tree-right-side-view.py
+++ 0199-binary-tree-right-side-view/0199-binary-tree-right-side-view.py
@@ -6,42 +6,19 @@
 #         self.right = right
 class Solution:
     def rightSideView(self, root: Optional[TreeNode]) -> List[int]:
-        temp = Solution.rec(self, root)
-        print(temp)
-        f = []
-        for i in temp:
-            f.append(i[0])
-        return f
-    def rec(self, root):
-        if root == None:
-            return []
-        temp = [root.val]
-        temp1 = Solution.rec(self, root.left)
-        temp2 = Solution.rec(self, root.right)
-        merge = []
-        merge.append(temp)
-        if temp2:
-            if temp1:
-                # only keep last when merged
-                temp1 = Solution.combine(self, temp1, temp2)
-                return merge + temp1
-            else:
-                return merge + temp2
-        else:
-            if temp1:
-                return merge + temp1
-            else:
-                return merge
-    def combine(self, list1, list2):
-        combined = []
-        max_len = max(len(list1), len(list2))
-
-        for i in range(max_len):
-            if i < len(list2):
-                combined.append(list2[i])
-            elif i < len(list1) and i < len(list2):
-                combined.append(list1[i] + list2[i])
-            else:
-                combined.append(list1[i])
-                
-        return combined
+        res = []
+        q = collections.deque()
+        q.append(root)
+        while q:
+            qlen = len(q)
+            last = q[-1]
+            if last:
+                res.append(last.val)
+            for i in range(qlen):
+                temp = q.popleft()
+                if temp:
+                    if temp.left:
+                        q.append(temp.left)
+                    if temp.right:
+                        q.append(temp.right)
+        return res
