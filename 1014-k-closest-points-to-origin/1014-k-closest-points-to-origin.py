c3a84fcdf0e728baecb0c0f59982abe326ad2293
Saideep Samineni <73835168+saideep2000@users.noreply.github.com>
2024-11-21 13:13:09 -0500

    Time: 74 ms (41.27%), Space: 21.5 MB (80.4%) - LeetHub

--- 1014-k-closest-points-to-origin/1014-k-closest-points-to-origin.py
+++ 1014-k-closest-points-to-origin/1014-k-closest-points-to-origin.py
@@ -3,7 +3,7 @@ class Solution:
         store = []
         heapq.heapify(store)
         for i in points:
-            temp = ((i[0]*i[0]) + (i[1]*i[1]))
+            temp = (i[0] ** 2) + (i[1] ** 2)
             heapq.heappush(store, (temp, i))
         f = []
         while k != 0:
