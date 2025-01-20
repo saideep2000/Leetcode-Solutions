1389397074534bc9f2f411b04bd56ef3f4037430
Saideep Samineni <73835168+saideep2000@users.noreply.github.com>
2025-01-20 17:47:53 -0500

    Time: 3 ms (5.32%), Space: 17.8 MB (40.97%) - LeetHub

---
+++ 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.py
@@ -0,0 +1,27 @@
+class Solution:
+    def letterCombinations(self, digits: str) -> List[str]:
+        final = []
+        if len(digits) == 0:
+            return final
+        tele = {
+            "2": ["a", "b", "c"],
+            "3": ["d", "e", "f"],
+            "4": ["g", "h", "i"],
+            "5": ["j", "k", "l"],
+            "6": ["m", "n", "o"],
+            "7": ["p", "q", "r", "s"],
+            "8": ["t", "u", "v"],
+            "9": ["w", "x", "y", "z"]
+        }
+
+        def backtrack(index, subsetStr):
+            if index == len(digits):
+                final.append(subsetStr)
+                return
+            curr = tele[digits[index]]
+            for i in range(0, len(curr)):
+                combo = copy.deepcopy(subsetStr)
+                combo = combo + curr[i]
+                backtrack(index+1, combo)
+        backtrack(0, "")
+        return final
