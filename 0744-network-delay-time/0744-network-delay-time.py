8195b063ac204fcb27d8bca343b67f75f0b2b923
Saideep Samineni <73835168+saideep2000@users.noreply.github.com>
2025-01-29 20:46:29 -0500

    Time: 416 ms (14.58%), Space: 20.1 MB (19.45%) - LeetHub

--- 0744-network-delay-time/0744-network-delay-time.py
+++ 0744-network-delay-time/0744-network-delay-time.py
@@ -2,6 +2,7 @@ class Solution:
     def networkDelayTime(self, times: List[List[int]], n: int, k: int) -> int:
         graph = defaultdict(list)
         visited = defaultdict(int)
+        m = 0
 
         for i, j, r in times:
             graph[i].append([j,r])
@@ -18,8 +19,9 @@ class Solution:
 
             for s,est in graph[now]:
                 heapq.heappush(qu, (est+time, s))
+            m = max(time, m)
 
         if len(visited) != n:
             return -1
 
-        return max(visited.values())
+        return m
