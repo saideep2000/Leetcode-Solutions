9f333507bfdbc8a3d727cc76e5942225dc25f63a
Saideep Samineni <73835168+saideep2000@users.noreply.github.com>
2025-02-11 17:30:15 -0500

    Time: 3 ms (52.22%), Space: 18.9 MB (48.09%) - LeetHub

--- 0001-two-sum/0001-two-sum.py
+++ 0001-two-sum/0001-two-sum.py
@@ -2,9 +2,7 @@ class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
         h = collections.defaultdict()
         for i in range(0,len(nums)):
+            com = target - nums[i]
+            if com in h:
+                return [i,h[com]]
             h[nums[i]] = i
-        print(h)
-        for i in range(0,len(nums)):
-            complement = target - nums[i]
-            if complement in h and i != h[complement]:
-                return [i, h[complement]]
