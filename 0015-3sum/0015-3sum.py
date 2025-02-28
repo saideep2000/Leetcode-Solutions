7f6c262c8f079b93bd1d7bcdab2c19991a0c54d1
Saideep Samineni <73835168+saideep2000@users.noreply.github.com>
2025-02-28 11:47:07 -0500

    Time: 623 ms (37.9%), Space: 20.9 MB (23.63%) - LeetHub

--- 0015-3sum/0015-3sum.py
+++ 0015-3sum/0015-3sum.py
@@ -1,19 +1,24 @@
 class Solution:
     def threeSum(self, nums: List[int]) -> List[List[int]]:
         nums = sorted(nums)
-        f = set()
-        
+        f = []
+        rem = float('inf')
         for k in range(0, len(nums)-2):
-            i, j = k+1, len(nums) - 1
-            target = nums[k] * -1
-            while i<j:
-                if nums[i] + nums[j] == target:
-                    f.add((nums[k], nums[i], nums[j]))
-                    i = i + 1
-                # if our number is too big bring down the right pointer down
-                elif nums[i] + nums[j] > target:
-                    j = j - 1
-                # or else bring the left one
-                else:
-                    i = i + 1
-        return list(f)
+            if nums[k] != rem:
+                i, j = k+1, len(nums) - 1
+                target = nums[k] * -1
+                while i<j:
+                    if nums[i] + nums[j] == target:
+                        f.append([nums[k], nums[i], nums[j]])
+                        i = i + 1
+                        j = j - 1
+                        while i<j and nums[i] == nums[i-1]:
+                            i = i + 1
+                    # if our number is too big bring down the right pointer down
+                    elif nums[i] + nums[j] > target:
+                        j = j - 1
+                    # or else bring the left one
+                    else:
+                        i = i + 1
+            rem = nums[k]
+        return f
