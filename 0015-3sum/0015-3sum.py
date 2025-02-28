f46d6870d26f81871226ea4a541a3ea605ee6abe
Saideep Samineni <73835168+saideep2000@users.noreply.github.com>
2025-02-27 21:36:07 -0500

    Time: 2289 ms (5.01%), Space: 20.3 MB (91.04%) - LeetHub

--- 0015-3sum/0015-3sum.py
+++ 0015-3sum/0015-3sum.py
@@ -6,7 +6,7 @@ class Solution:
         for k in range(0, len(nums)-2):
             i, j = k+1, len(nums) - 1
             target = nums[k] * -1
-            while i<j and j:
+            while i<j:
                 if nums[i] + nums[j] == target:
                     f.add((nums[k], nums[i], nums[j]))
                     i = i + 1
