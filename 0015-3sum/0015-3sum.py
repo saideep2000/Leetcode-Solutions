e9e0d2d2d4c37bdccdb81b736ee55dda20a65501
Saideep Samineni <73835168+saideep2000@users.noreply.github.com>
2025-02-27 21:35:48 -0500

    Time: 2077 ms (5.01%), Space: 20.2 MB (91.04%) - LeetHub

--- 0015-3sum/0015-3sum.py
+++ 0015-3sum/0015-3sum.py
@@ -1,16 +1,19 @@
 class Solution:
     def threeSum(self, nums: List[int]) -> List[List[int]]:
         nums = sorted(nums)
-        store = set()
-        for i in range(0,len(nums)):
-            j,k = i+1 ,len(nums)-1
-            while j < k:
-                if i == j or nums[i] + nums[j] + nums[k] < 0:
-                    j = j + 1
-                elif i == k or nums[i] + nums[j] + nums[k] > 0:
-                    k = k - 1
+        f = set()
+        
+        for k in range(0, len(nums)-2):
+            i, j = k+1, len(nums) - 1
+            target = nums[k] * -1
+            while i<j and j:
+                if nums[i] + nums[j] == target:
+                    f.add((nums[k], nums[i], nums[j]))
+                    i = i + 1
+                # if our number is too big bring down the right pointer down
+                elif nums[i] + nums[j] > target:
+                    j = j - 1
+                # or else bring the left one
                 else:
-                    trip = sorted([nums[i],nums[j], nums[k]])
-                    store.add(tuple(trip))
-                    j,k = j+1,k-1
-        return store
+                    i = i + 1
+        return list(f)
