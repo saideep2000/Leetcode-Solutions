97e768886f0a3cebdab1dc17ccbcdb9ff5a8687e
Saideep Samineni <73835168+saideep2000@users.noreply.github.com>
2025-03-16 09:58:41 -0400

    Time: 250 ms (56.35%), Space: 20.4 MB (44.7%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.py
+++ 0200-number-of-islands/0200-number-of-islands.py
@@ -1,22 +1,24 @@
 class Solution:
     def numIslands(self, grid: List[List[str]]) -> int:
-        m = len(grid)
-        n = len(grid[0])
-        def backtrack(p, q):
-            if p < 0 or p >= m or q < 0 or q >= n or grid[p][q] == "0":
-                return
-            moves = [(0, 1), (1, 0), (0, -1), (-1, 0)]
+        moves = [[0,1], [1,0], [-1,0], [0,-1]]
+        islands = 0
+        n = len(grid)
+        m = len(grid[0])
 
-            grid[p][q] = "0"
+        def dfs(self, row, col):
+            # make the cell visited with -1
+            grid[row][col] = -1
 
-            for dx, dy in moves:
-                backtrack(p + dx, q + dy)
-            return
-
-        f = 0
-        for i in range(0,m):
-            for j in range(0, n):
+            # we will search for all 4 possible directions
+            for mi, mj in moves:
+                if row+mi >= 0 and col+mj >= 0 and row+mi < n and col+mj < m:
+                    if grid[row+mi][col+mj] == "1":
+                        dfs(self, row+mi, col+mj)
+        
+        # we'll itreate through all the islands i.e; 1
+        for i in range(n):
+            for j in range(m):
                 if grid[i][j] == "1":
-                    backtrack(i, j)
-                    f = f + 1
-        return f
+                    dfs(self, i, j)
+                    islands = islands + 1
+        return islands
