eefa64447c2212addb6717d1a49385329d5418eb
Saideep Samineni <73835168+saideep2000@users.noreply.github.com>
2025-03-17 15:06:40 -0400

    Time: 57 ms (5.28%), Space: 31 MB (5.54%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.py
+++ 0049-group-anagrams/0049-group-anagrams.py
@@ -1,18 +1,34 @@
+class TrieNode():
+    def __init__(self):
+        self.children = {}
+        self.words = []
+class Trie():
+    def __init__(self):
+        self.root = TrieNode()
+    def insert(self, lis, original_str):
+        node = self.root
+        # ['a', 'e', 't']
+        for each in lis:
+            if each not in node.children:
+                node.children[each] = TrieNode()
+            node = node.children[each]
+        node.words.append(original_str)
+    def collect_words(self):
+        lst = []
+
+        def dfs(node):
+            if node.words:
+                lst.append(node.words)
+            for child in node.children.values():
+                dfs(child)
+            
+        dfs(self.root)
+        return lst
+
 class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
-        hm = defaultdict(list)
-
-    
-        def get_key(s):
-            count = [0] * 26
-            for char in s:
-                count[ord(char) - ord('a')] += 1
-            return tuple(count)
-
-        
-
+        trie = Trie()
         for each in strs:
-            key = get_key(each)
-            hm[key].append(each)
-        
-        return list(hm.values())
+            temp = sorted(each)
+            trie.insert(temp, each)
+        return trie.collect_words()
