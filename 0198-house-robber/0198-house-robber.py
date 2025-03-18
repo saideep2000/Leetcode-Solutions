44e9cf42e40ab70c3973b473831fb64341c936c2
Saideep Samineni <73835168+saideep2000@users.noreply.github.com>
2025-03-18 18:59:09 -0400

    Time: 0 ms (100%), Space: 17.8 MB (54.49%) - LeetHub

--- 0198-house-robber/0198-house-robber.py
+++ 0198-house-robber/0198-house-robber.py
@@ -3,18 +3,15 @@ class Solution:
         # In -> [11, 50, 100,  2, 2, 100, 11] -> Out
         # In -> [0,   1,   2,  3, 4,   5,  6] -> Out
 
-
-
-        # maxRobbedAmount = [None, None, None, None, None, None, None, None]
-        maxRobbedAmount = [None for _ in range(len(nums) + 1)]
-
+    
         # N = 7
         N = len(nums)
 
-        # maxRobbedAmount = [None, None, None, None, None, None, 11, 0]
-        maxRobbedAmount[N], maxRobbedAmount[N - 1] = 0, nums[N - 1]
+        one, two = 0, nums[N - 1]
 
         for each in range(N-2, -1, -1):
-            maxRobbedAmount[each] = max(maxRobbedAmount[each + 1], maxRobbedAmount[each+2] + nums[each])
+            temp = max(two, one + nums[each])
+            one = two
+            two = temp
         
-        return maxRobbedAmount[0]
+        return two
