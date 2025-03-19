ca18c5d54aa7ac3fbadad4ed979fdcb14e44eae2
Saideep Samineni <73835168+saideep2000@users.noreply.github.com>
2025-03-19 14:10:45 -0400

    Time: 0 ms (100%), Space: 17.7 MB (59.21%) - LeetHub

--- 0213-house-robber-ii/0213-house-robber-ii.py
+++ 0213-house-robber-ii/0213-house-robber-ii.py
@@ -1,23 +1,20 @@
 class Solution:
     def rob(self, nums: List[int]) -> int:
-        return max(nums[0], self.helper(nums[1:]), self.helper(nums[:-1]))
-    def helper(self,arr):
-        hm1 = {}
-        def dp1(n):
-            if n < 0:
-                return 0
-            if 0 <= n <= 1:
-                hm1[n] = arr[n]
-                return arr[n]
-            ans = 0
-            for i in range(0,n-1):
-                if i not in hm1:
-                    ans = max(ans, dp1(i))
-                else:
-                    ans = max(ans, hm1[i])
-            ans = ans + arr[n]
-            hm1[n] = ans
-            return ans
-        
-        n = len(arr)
-        return max(dp1(n-1), dp1(n-2))
+        if len(nums) == 1:
+            return nums[0]
+
+        def house_robber_i(num):
+            n = len(num)
+            one, two = 0, num[n-1]
+            for i in range(n-2,-1,-1):
+                now = max(one + num[i], two)
+                one = two
+                two = now
+            return two
+
+        temp, temp2 = house_robber_i(nums[:len(nums)-1]), house_robber_i(nums[1:len(nums)])
+        return max(temp, temp2)
+
+        #  n = 4
+        # [2,3,0]
+        # [0,1,2,3,4]
