881a99a62a816ca978c549980416f06557bbdf13
Saideep Samineni <73835168+saideep2000@users.noreply.github.com>
2025-03-20 10:56:51 -0400

    Time: 6355 ms (5%), Space: 19.3 MB (50.25%) - LeetHub

--- 0210-course-schedule-ii/0210-course-schedule-ii.py
+++ 0210-course-schedule-ii/0210-course-schedule-ii.py
@@ -1,28 +1,25 @@
 class Solution:
     def findOrder(self, numCourses: int, prerequisites: List[List[int]]) -> List[int]:
-        graph = defaultdict(list)
+        rule = defaultdict(list)
         for course, pre in prerequisites:
-            graph[course].append(pre)
-        
+            rule[course].append(pre)
+
+        visited = [0]*numCourses
         final = []
 
-        visited = [0] * numCourses
-
-        def dfs(x):
-            if visited[x] == 1:
+        def dfs(course) -> bool:
+            if visited[course] == 1:
                 return False
-            visited[x] = 1
-            for n in graph[x]:
-                if not dfs(n):
+            visited[course] = 1
+            for each in rule[course]:
+                if not dfs(each):
                     return False
-            if x not in final:
-                final.append(x)
-            visited[x] = 2
+            if course not in final:
+                final.append(course)
+            visited[course] = 2
             return True
-
+        
         for i in range(0,numCourses):
-            if i not in final:
-                if not dfs(i):
-                    return []
-
+            if not dfs(i):
+                return []
         return final
