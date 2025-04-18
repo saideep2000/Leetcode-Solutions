5339d1a23960a84e54b96473f1b0eef80bc517ad
Saideep Samineni <73835168+saideep2000@users.noreply.github.com>
2025-04-18 08:02:36 -0400

    Time: 0 ms (100%), Space: 18.3 MB (25.99%) - LeetHub

--- 0074-search-a-2d-matrix/0074-search-a-2d-matrix.py
+++ 0074-search-a-2d-matrix/0074-search-a-2d-matrix.py
@@ -1,25 +1,18 @@
 class Solution:
-    def binarySearch(target, li):
-        l = 0
-        r = len(li)-1
-        while l <= r :
-            m = (l+r)//2
-            if li[m] == target:
-                return True
-            elif li[m] > target:
-                r = m - 1
-            else:
-                l = m + 1
-        return False
     def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
-        top = 0
-        bottom = len(matrix)-1
-        while top <= bottom:
-            row = (top+bottom)//2
-            if target >= matrix[row][0] and target <= matrix[row][-1]:
-                return Solution.binarySearch(target, matrix[row])
-            elif matrix[row][0] > target:
-                bottom = row - 1
-            else:
-                top = row + 1
-        return False 
+        def bfs(li):
+            l,r = 0, len(li)-1
+            while l <= r:
+                middle = l + ((r-l)//2)
+                if li[middle] == target:
+                    return True
+                elif li[middle] > target:
+                    r = middle - 1
+                elif li[middle] < target:
+                    l = middle + 1
+            return False
+        
+        for each_list in matrix:
+            if target >= each_list[0] and target <= each_list[-1]:
+                return bfs(each_list)
+        return False
