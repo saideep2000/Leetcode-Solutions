76f08311875d671fe48991aabb3f9de47eb8b5f8
Saideep Samineni <73835168+saideep2000@users.noreply.github.com>
2025-05-03 08:58:49 -0400

    Time: 4 ms (41.14%), Space: 19.5 MB (6.32%) - LeetHub

--- 0001-two-sum/0001-two-sum.py
+++ 0001-two-sum/0001-two-sum.py
@@ -1,8 +1,11 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        h = collections.defaultdict()
+        store = defaultdict()
+
         for i in range(0,len(nums)):
-            com = target - nums[i]
-            if com in h:
-                return [i,h[com]]
-            h[nums[i]] = i
+            rem = nums[i]
+            if rem in store:
+                return [store[rem], i]
+            rem = target-nums[i]
+            store[rem] = i
+
