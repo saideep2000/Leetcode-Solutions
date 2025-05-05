10f295c61d102e43d8e228ac3b19af4df19a73a8
Saideep Samineni <73835168+saideep2000@users.noreply.github.com>
2025-05-05 12:35:49 -0400

    Time: 23 ms (63.83%), Space: 23.3 MB (53.86%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.py
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.py
@@ -1,37 +1,14 @@
 class Solution:
     def productExceptSelf(self, nums: List[int]) -> List[int]:
-        out = len(nums)*[0]
-        pre = 1
-        post = 1
-        for i in range(0,len(nums)):
-            out[i] = pre
-            pre = pre * nums[i]
-        for i in range(len(nums)-1, -1, -1):
-            out[i] = out[i] * post
-            post = post * nums[i]
-        return out
-        # right_pass = len(nums)*[0]
-        # left_pass = len(nums)*[0]
-        # out = len(nums)*[0]
-        # for i in range(0,len(nums)-1):
-        #     if i == 0:
-        #         right_pass[i] = nums[i]
-        #     else:
-        #         right_pass[i] = right_pass[i-1] * nums[i]
+        final = [1]*len(nums)
+
+        for pos in range(1,len(nums)):
+            final[pos] = nums[pos-1] * final[pos-1]
+        temp = 1
         
-        # for i in range(len(nums)-1, 0, -1):
-        #     print(i)
-        #     if i == (len(nums)-1):
-        #         left_pass[i] = nums[i]
-        #     else:
-        #         left_pass[i] = left_pass[i+1] * nums[i]
-        # print(right_pass)
-        # print(left_pass)
-        # for i in range(0,len(nums)):
-        #     if i == 0:
-        #         out[0] = left_pass[1]
-        #     elif i == (len(nums)-1):
-        #         out[i] = right_pass[i-1]
-        #     else:
-        #         out[i] = right_pass[i-1]*left_pass[i+1]
-        # return out
+        for pos in range(len(nums)-2, -1, -1):
+            temp = temp * nums[pos+1]
+            final[pos] = final[pos] * temp
+            
+
+        return final
