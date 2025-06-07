326685ea1a96a34b7d071c1ca3f789412cf09efd
Saideep Samineni <73835168+saideep2000@users.noreply.github.com>
2025-06-07 18:31:33 -0400

    Time: 11 ms (36.24%), Space: 17.9 MB (83.32%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.py
+++ 0125-valid-palindrome/0125-valid-palindrome.py
@@ -1,23 +1,22 @@
 class Solution:
-    def check_for_alphanum(o):
+    def check_valid_char(o):
         if (65 <= o <= 90) or (97 <= o <= 122) or (48 <= o <= 57):
-            return False
-        else:
             return True
+        else:
+            return False
+
     def isPalindrome(self, s: str) -> bool:
-        i, j = 0, len(s)-1
+        i,j = 0, len(s)-1
+
         while i<j:
-            if Solution.check_for_alphanum(ord(s[i])):
+            if Solution.check_valid_char(ord(s[i])) == False:
                 i = i + 1
-            elif Solution.check_for_alphanum(ord(s[j])):
+            elif Solution.check_valid_char(ord(s[j])) == False:
                 j = j - 1
             else:
-                print(s[i], s[j])
                 if s[i].lower() != s[j].lower():
                     return False
                 else:
                     i = i + 1
                     j = j - 1
         return True
-
-    
