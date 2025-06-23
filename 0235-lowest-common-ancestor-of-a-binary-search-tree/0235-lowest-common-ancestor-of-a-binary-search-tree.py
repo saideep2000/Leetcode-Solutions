531b4b36abb121b833e1d23224e434c66c627a14
Saideep Samineni <73835168+saideep2000@users.noreply.github.com>
2025-06-23 19:26:25 -0400

    Time: 50 ms (92.44%), Space: 21 MB (47.39%) - LeetHub

--- 0235-lowest-common-ancestor-of-a-binary-search-tree/0235-lowest-common-ancestor-of-a-binary-search-tree.py
+++ 0235-lowest-common-ancestor-of-a-binary-search-tree/0235-lowest-common-ancestor-of-a-binary-search-tree.py
@@ -8,33 +8,41 @@
 class Solution:
     def lowestCommonAncestor(self, root: 'TreeNode', p: 'TreeNode', q: 'TreeNode') -> 'TreeNode':
 
-        def traverseTree(head):
-            if head == None:
-                return (False, None)
+        # def traverseTree(head):
+        #     if head == None:
+        #         return (False, None)
             
-            found_l, lca_l = traverseTree(head.left)
-            found_r, lca_r = traverseTree(head.right)
+        #     found_l, lca_l = traverseTree(head.left)
+        #     found_r, lca_r = traverseTree(head.right)
 
-            if lca_l or lca_r:
-                if lca_l != None:
-                    return (True, lca_l)
-                else:
-                    return (True, lca_r)
+        #     if lca_l or lca_r:
+        #         if lca_l != None:
+        #             return (True, lca_l)
+        #         else:
+        #             return (True, lca_r)
             
-            if found_l and found_r:
-                return (True, head)
+        #     if found_l and found_r:
+        #         return (True, head)
             
-            if found_l or found_r:
-                if head == p or head == q:
-                    return (True, head)
-                return (True, None)
+        #     if found_l or found_r:
+        #         if head == p or head == q:
+        #             return (True, head)
+        #         return (True, None)
             
-            if head == p or head == q:
-                return (True, None)
+        #     if head == p or head == q:
+        #         return (True, None)
             
-            return (False, None)
+        #     return (False, None)
 
         
-        found, result = traverseTree(root)
+        # found, result = traverseTree(root)
 
-        return result
+        # return result
+
+        while root:
+            if root.val > p.val and root.val > q.val:
+                root = root.left
+            elif root.val < p.val and root.val < q.val:
+                root = root.right
+            else:
+                return root
