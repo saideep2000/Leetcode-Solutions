1ea503d7f709816f4f4898017324cd420e59852f
Saideep Samineni <73835168+saideep2000@users.noreply.github.com>
2025-06-23 19:19:32 -0400

    Time: 353 ms (5.86%), Space: 21.4 MB (5.45%) - LeetHub

--- 0235-lowest-common-ancestor-of-a-binary-search-tree/0235-lowest-common-ancestor-of-a-binary-search-tree.py
+++ 0235-lowest-common-ancestor-of-a-binary-search-tree/0235-lowest-common-ancestor-of-a-binary-search-tree.py
@@ -7,21 +7,37 @@
 
 class Solution:
     def lowestCommonAncestor(self, root: 'TreeNode', p: 'TreeNode', q: 'TreeNode') -> 'TreeNode':
-        while root:
-            temp = root.val
-            if root.val > p.val:
-                if root.val < q.val:
-                    return root
-                elif root.val > q.val:
-                    root = root.left
+
+        def traverseTree(head):
+            if head == None:
+                return (False, None)
+            
+            found_l, lca_l = traverseTree(head.left)
+            found_r, lca_r = traverseTree(head.right)
+
+            print("found_l & lca_l & ", head.val, found_l, lca_l)
+            print("found_r & lca_r & ", head.val, found_r, lca_r)
+
+            if lca_l or lca_r:
+                if lca_l != None:
+                    return (True, lca_l)
                 else:
-                    return root
-            elif root.val < p.val:
-                if root.val > q.val:
-                    return root
-                elif root.val < q.val:
-                    root = root.right
-                else:
-                    return root
-            else:
-                return root
+                    return (True, lca_r)
+            
+            if found_l and found_r:
+                return (True, head)
+            
+            if found_l or found_r:
+                if head == p or head == q:
+                    return (True, head)
+                return (True, None)
+            
+            if head == p or head == q:
+                return (True, None)
+            
+            return (False, None)
+
+        
+        found, result = traverseTree(root)
+
+        return result
