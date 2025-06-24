e766763fafa0d777c7ac6971f53a9fa66d749754
Saideep Samineni <73835168+saideep2000@users.noreply.github.com>
2025-06-23 20:38:48 -0400

    Time: 3 ms (20.12%), Space: 18.5 MB (59.41%) - LeetHub

--- 0102-binary-tree-level-order-traversal/0102-binary-tree-level-order-traversal.py
+++ 0102-binary-tree-level-order-traversal/0102-binary-tree-level-order-traversal.py
@@ -6,31 +6,50 @@
 #         self.right = right
 class Solution:
     def levelOrder(self, root: Optional[TreeNode]) -> List[List[int]]:
-        # [[2,3], [4,5]] and [[1,0], [7,8]]
-        def merge2lists(l1, l2):
-            reverse = False
-            if len(l1) < len(l2):
-                l1, l2 = l2, l1
-                reverse = True
-            for i in range(len(l2)):
-                if l2[i] != []:
-                    if reverse:
-                        l1[i] = l2[i] + l1[i]
-                    else:
-                        l1[i].extend(l2[i])
+        if not root:  # Handle edge case
+            return []
             
-            return l1
+        result = []
+        store = deque([root])
 
-        def traverseTree(head):
-            if head == None:
-                return []
-            l_left = traverseTree(head.left)
-            l_right = traverseTree(head.right)
+        while store:
+            initial = []
             temp = []
-            temp.append([head.val])
-            if l_left != [] or l_right != []:
-                temp.extend(merge2lists(l_left, l_right))
-            
-            return temp
+            for i in range(len(store)):
+                initial.append(store[i].val)
+                if store[i].left != None:
+                    temp.append(store[i].left)
+                if store[i].right != None:
+                    temp.append(store[i].right)
+            store = temp
+            result.append(initial)
+        return result
 
-        return traverseTree(root)
+        # [[2,3], [4,5]] and [[1,0], [7,8]]
+        # def merge2lists(l1, l2):
+        #     reverse = False
+        #     if len(l1) < len(l2):
+        #         l1, l2 = l2, l1
+        #         reverse = True
+        #     for i in range(len(l2)):
+        #         if l2[i] != []:
+        #             if reverse:
+        #                 l1[i] = l2[i] + l1[i]
+        #             else:
+        #                 l1[i].extend(l2[i])
+            
+        #     return l1
+
+        # def traverseTree(head):
+        #     if head == None:
+        #         return []
+        #     l_left = traverseTree(head.left)
+        #     l_right = traverseTree(head.right)
+        #     temp = []
+        #     temp.append([head.val])
+        #     if l_left != [] or l_right != []:
+        #         temp.extend(merge2lists(l_left, l_right))
+            
+        #     return temp
+
+        # return traverseTree(root)
