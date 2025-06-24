c065c9a5dcc8c7a2795027783e9c87744600bc90
Saideep Samineni <73835168+saideep2000@users.noreply.github.com>
2025-06-24 15:03:47 -0400

    Time: 0 ms (100%), Space: 19.7 MB (67.96%) - LeetHub

--- 0098-validate-binary-search-tree/0098-validate-binary-search-tree.py
+++ 0098-validate-binary-search-tree/0098-validate-binary-search-tree.py
@@ -6,17 +6,25 @@
 #         self.right = right
 class Solution:
     def isValidBST(self, root: Optional[TreeNode]) -> bool:
-        minn = float('-inf')
-        maxx = float('inf')
-        return Solution.rec(self, root, minn, maxx)
-    def rec(self, root, minn, maxx):
-        if root == None:
-            return True
-        if root.val >= maxx or root.val <= minn:
-            return False
-        temp1 = Solution.rec(self, root.left, minn, root.val)
-        temp2 = Solution.rec(self, root.right, root.val, maxx)
-        if temp1 and temp2:
-            return True
-        else:
-            return False
+        
+        def traverseTree(head):
+            if head == None:
+                return (True, None, None)
+            
+            status_l, min_val_l, max_val_l = traverseTree(head.left)
+            status_r, min_val_r, max_val_r = traverseTree(head.right)
+
+            if not status_l or not status_r:
+                return (False, min_val_l, max_val_r)
+            
+            if max_val_l != None:
+                if head.val <= max_val_l:
+                    return (False, min_val_l, max_val_r)
+            if min_val_r != None:
+                if head.val >= min_val_r:
+                    return (False, min_val_l, max_val_r)
+            
+
+            return (True, min_val_l if min_val_l else head.val, max_val_r if max_val_r else head.val)
+        status, min_val, max_val = traverseTree(root)
+        return status
