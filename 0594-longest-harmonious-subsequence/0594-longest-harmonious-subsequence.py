60a43352ecda74c9a0eea4c1d98574f238063903
Saideep Samineni <73835168+saideep2000@users.noreply.github.com>
2025-06-30 13:49:37 -0400

    Time: 79 ms (5.39%), Space: 19.1 MB (46.65%) - LeetHub

--- 0594-longest-harmonious-subsequence/0594-longest-harmonious-subsequence.py
+++ 0594-longest-harmonious-subsequence/0594-longest-harmonious-subsequence.py
@@ -1,17 +1,14 @@
 class Solution:
     def findLHS(self, nums: List[int]) -> int:
+        result = 0
         store_counts = defaultdict(int)
         for i in range(0,len(nums)):
             store_counts[nums[i]] = store_counts[nums[i]] + 1
-        sorted_store = dict(sorted(store_counts.items()))
-
-        previous = None
-        result = 0
-        for key, value in sorted_store.items():
-            if previous == None:
-                previous = key
-            else:
-                if key - previous == 1:
-                    result = max(result , sorted_store[previous] + value)
-                previous = key
+            if nums[i] + 1 in store_counts:
+                temp = store_counts[nums[i]] + store_counts[nums[i] + 1]
+                result = max(result, temp)
+            if nums[i] - 1 in store_counts:
+                temp = store_counts[nums[i]] + store_counts[nums[i] - 1]
+                result = max(result, temp)
+        
         return result
