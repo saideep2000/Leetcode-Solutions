311fd78eaccd683d2d4363f8d9d1190674fc0143
Saideep Samineni <73835168+saideep2000@users.noreply.github.com>
2025-07-18 15:58:18 -0400

    Time: 134 ms (63.45%), Space: 77.9 MB (96.85%) - LeetHub

--- 0146-lru-cache/0146-lru-cache.py
+++ 0146-lru-cache/0146-lru-cache.py
@@ -1,61 +1,56 @@
-class DoubleLinkedList:
-    def __init__(self, key, value=None):
+class ListNode:
+    def __init__(self, key = 0, val = 0, prev = None, next = None):
         self.key = key
-        self.value = value
-        self.prev = None
-        self.next = None
-
+        self.val = val
+        self.prev = prev
+        self.next = next
 
 class LRUCache:
+
     def __init__(self, capacity: int):
-        self.cap = capacity
-        self.hm = {}  # Dictionary: key -> (value, node)
-        
-        # Dummy head & tail for easier operations
-        self.head = DoubleLinkedList(0)  
-        self.tail = DoubleLinkedList(0)  
+        self.head = ListNode(0, 0, None, None)
+        self.tail = ListNode(0, 0, self.head, None)
         self.head.next = self.tail
-        self.tail.prev = self.head
-
-    def _remove(self, node):
-        """Removes a node from the doubly linked list"""
-        prev_node = node.prev
-        next_node = node.next
-        prev_node.next = next_node
-        next_node.prev = prev_node
-
-    def _add_to_front(self, node):
-        """Moves the node to the front (MRU position)"""
-        node.next = self.head.next
-        node.prev = self.head
-        self.head.next.prev = node
-        self.head.next = node
+        
+        self.hm = {}
+        self.capacity = capacity
 
     def get(self, key: int) -> int:
         if key in self.hm:
-            value, node = self.hm[key]
-            
-            # Move to the front only if it's not already there
-            if self.head.next != node:  
-                self._remove(node)
-                self._add_to_front(node)
-            
-            return value
-        return -1
-
+            # Make the node most recently used (move to front)
+            temp = self.hm[key]
+            self.delNode(temp)
+            self.addToFront(temp)
+            return temp.val
+        else:
+            return -1
+        
     def put(self, key: int, value: int) -> None:
         if key in self.hm:
-            # If key exists, remove the old node
-            _, node = self.hm[key]
-            self._remove(node)
+            # Update existing key
+            temp = self.hm[key]
+            self.delNode(temp)
+            temp.val = value
+            self.addToFront(temp)
+        else:
+            # Add new key
+            if len(self.hm) >= self.capacity:
+                # Remove least recently used (node before tail)
+                lru_node = self.tail.prev
+                self.delNode(lru_node)
+                del self.hm[lru_node.key]
+            
+            # Create new node and add to front
+            new_node = ListNode(key, value)
+            self.addToFront(new_node)
+            self.hm[key] = new_node
 
-        # Add a new node to the front
-        new_node = DoubleLinkedList(key, value)
-        self._add_to_front(new_node)
-        self.hm[key] = (value, new_node)  # Store in hashmap
-
-        if len(self.hm) > self.cap:
-            # Remove the LRU node (just before tail)
-            lru_node = self.tail.prev
-            self._remove(lru_node)
-            del self.hm[lru_node.key]  # Delete from hashmap
+    def delNode(self, node):
+        node.prev.next = node.next
+        node.next.prev = node.prev
+    
+    def addToFront(self, node):
+        node.prev = self.head
+        node.next = self.head.next
+        self.head.next.prev = node
+        self.head.next = node
