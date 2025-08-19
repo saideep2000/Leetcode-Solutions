e8744f0ca5fc31e99b21c653e9599281263fcf68
Saideep Samineni <73835168+saideep2000@users.noreply.github.com>
2025-08-19 00:24:49 -0400

    Time: 40 ms (45.44%), Space: 28.4 MB (83.4%) - LeetHub

--- 2432-number-of-zero-filled-subarrays/2432-number-of-zero-filled-subarrays.py
+++ 2432-number-of-zero-filled-subarrays/2432-number-of-zero-filled-subarrays.py
@@ -1,35 +1,12 @@
 class Solution:
     def zeroFilledSubarray(self, nums: List[int]) -> int:
-        result = []
+        left, right = 0, 0
         n = len(nums)
-
-        flag = 0
-        counter = 0
-
-        for i in range(n):
-            if nums[i] == 0:
-                if flag == 0:
-                    flag = 1
-
-                counter += 1
+        result = 0
+        for right in range(n):
+            if nums[right] != 0:
+                left = right + 1
             else:
-                if flag == 1:
-                    result.append(counter)
-                    flag = 0
-                    counter = 0
-            
-        if flag == 1:
-            result.append(counter)
-            flag = 0
-            counter = 0
-    
-        final = 0
+                result = result + (right - left + 1)
 
-        for i in range(len(result)):
-            each = result[i]
-            temp = (each) * (each + 1) // 2
-            final = final + temp
-
-        return final
-            
-
+        return result
