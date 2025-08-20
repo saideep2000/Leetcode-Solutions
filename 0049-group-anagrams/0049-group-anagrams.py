8f106e929068b4408ab14e383c32e7289f183aad
Saideep Samineni <73835168+saideep2000@users.noreply.github.com>
2025-08-20 16:40:11 -0400

    Time: 55 ms (5.38%), Space: 30.7 MB (5.04%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.py
+++ 0049-group-anagrams/0049-group-anagrams.py
@@ -1,34 +1,36 @@
-class TrieNode():
+from typing import List
+from collections import defaultdict
+
+class TrieNode:
     def __init__(self):
         self.children = {}
-        self.words = []
-class Trie():
+        self.word = []
+
+class Trie:
     def __init__(self):
         self.root = TrieNode()
-    def insert(self, lis, original_str):
-        node = self.root
-        # ['a', 'e', 't']
-        for each in lis:
-            if each not in node.children:
-                node.children[each] = TrieNode()
-            node = node.children[each]
-        node.words.append(original_str)
-    def collect_words(self):
-        lst = []
 
+    def addWord(self, w: str):
+        head = self.root
+        for ch in sorted(w):
+            if ch not in head.children:
+                head.children[ch] = TrieNode()
+            head = head.children[ch]
+        head.word.append(w)
+
+    def get(self):
+        result = []
         def dfs(node):
-            if node.words:
-                lst.append(node.words)
+            if node.word:
+                result.append(node.word)
             for child in node.children.values():
                 dfs(child)
-            
         dfs(self.root)
-        return lst
+        return result
 
 class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
         trie = Trie()
-        for each in strs:
-            temp = sorted(each)
-            trie.insert(temp, each)
-        return trie.collect_words()
+        for s in strs:
+            trie.addWord(s)
+        return trie.get()
